3d3681fb66e5f50d22341e33de915362599fc978
Todd Stoffel <toddstoffel@icloud.com>
2021-01-23 11:30:20 -0800

    Reformat

--- queries/2018_airline_summary.sql
+++ queries/2018_airline_summary.sql
@@ -1,17 +1,16 @@
-select
-q.airline,
-q.volume flight_count,
-round(100 * q.volume / sum(q.volume) over (order by q.airline rows between unbounded preceding and unbounded following),2) market_share_pct,
-round(100 * (q.cancelled / q.volume), 2) cancelled_pct,
-round(100 * (q.diverted / q.volume), 2) diverted_pct
-from (
-    select a.airline,
-    count(*) volume,
-    sum(diverted) diverted,
-    sum(cancelled) cancelled
-    from flights f
-    join airlines a on f.carrier = a.iata_code
-    where f.year = 2018
-    group by a.airline
-) q
-order by flight_count desc;
+SELECT   `q`.`airline`,
+         `q`.`volume`                                                                                                                                  AS `flight_count`,
+         ROUND(100 * `q`.`volume` / SUM(`q`.`volume`) OVER ( ORDER BY `q`.`airline` ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING), 2)      AS `market_share_pct`,
+         ROUND(100 * ( q.cancelled / q.volume ), 2)                                                                                                    AS `cancelled_pct`,
+         ROUND(100 * ( q.diverted / q.volume ), 2)                                                                                                     AS `diverted_pct`
+FROM     (
+                  SELECT   `a`.`airline`,
+                           COUNT(*)       AS `volume`,
+                           SUM(`diverted`)  AS `diverted`,
+                           SUM(`cancelled`) AS `cancelled`
+                  FROM     `flights` `f`
+                  JOIN     `airlines` `a`
+                  ON       `f`.`carrier` = `a`.`iata_code`
+                  WHERE    `f`.`year` = 2018
+                  GROUP BY `a`.`airline` ) AS `q`
+ORDER BY `flight_count` DESC;
