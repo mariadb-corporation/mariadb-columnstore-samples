d64246a54b76ff4dec669dff315d75a425add4a9
Todd Stoffel <toddstoffel@icloud.com>
2021-02-06 23:26:55 -0800

    Format SQL

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,55 +1,64 @@
-drop database if exists bts;
+DROP DATABASE IF EXISTS bts;
 
-create database bts;
+CREATE DATABASE bts;
 
-use bts;
+USE bts;
 
-create table airlines (
-iata_code varchar(2),
-airline varchar(30)
-) engine=columnstore default character set=utf8;
+CREATE TABLE airlines
+  (
+     iata_code VARCHAR(2),
+     airline   VARCHAR(30)
+  )
+engine=columnstore
+DEFAULT CHARACTER SET=utf8;
 
-create table airports (
-iata_code varchar(3),
-airport varchar(80),
-city varchar(30),
-state varchar(2),
-country varchar(30),
-latitude decimal(11,4),
-longitude decimal(11,4)
-) engine=columnstore default character set=utf8;
+CREATE TABLE airports
+  (
+     iata_code VARCHAR(3),
+     airport   VARCHAR(80),
+     city      VARCHAR(30),
+     state     VARCHAR(2),
+     country   VARCHAR(30),
+     latitude  DECIMAL(11, 4),
+     longitude DECIMAL(11, 4)
+  )
+engine=columnstore
+DEFAULT CHARACTER SET=utf8;
 
-create table flights (
-year smallint,
-month tinyint,
-day tinyint,
-day_of_week tinyint,
-fl_date date,
-carrier varchar(2),
-tail_num varchar(6),
-fl_num smallint,
-origin varchar(5),
-dest varchar(5),
-crs_dep_time varchar(4),
-dep_time varchar(4),
-dep_delay smallint,
-taxi_out smallint,
-wheels_off varchar(4),
-wheels_on varchar(4),
-taxi_in smallint,
-crs_arr_time varchar(4),
-arr_time varchar(4),
-arr_delay smallint,
-cancelled smallint,
-cancellation_code smallint,
-diverted smallint,
-crs_elapsed_time smallint,
-actual_elapsed_time smallint,
-air_time smallint,
-distance smallint,
-carrier_delay smallint,
-weather_delay smallint,
-nas_delay smallint,
-security_delay smallint,
-late_aircraft_delay smallint
-) engine=columnstore default character set=utf8;
+CREATE TABLE flights
+  (
+     year                SMALLINT,
+     month               TINYINT,
+     day                 TINYINT,
+     day_of_week         TINYINT,
+     fl_date             DATE,
+     carrier             VARCHAR(2),
+     tail_num            VARCHAR(6),
+     fl_num              SMALLINT,
+     origin              VARCHAR(5),
+     dest                VARCHAR(5),
+     crs_dep_time        VARCHAR(4),
+     dep_time            VARCHAR(4),
+     dep_delay           SMALLINT,
+     taxi_out            SMALLINT,
+     wheels_off          VARCHAR(4),
+     wheels_on           VARCHAR(4),
+     taxi_in             SMALLINT,
+     crs_arr_time        VARCHAR(4),
+     arr_time            VARCHAR(4),
+     arr_delay           SMALLINT,
+     cancelled           SMALLINT,
+     cancellation_code   SMALLINT,
+     diverted            SMALLINT,
+     crs_elapsed_time    SMALLINT,
+     actual_elapsed_time SMALLINT,
+     air_time            SMALLINT,
+     distance            SMALLINT,
+     carrier_delay       SMALLINT,
+     weather_delay       SMALLINT,
+     nas_delay           SMALLINT,
+     security_delay      SMALLINT,
+     late_aircraft_delay SMALLINT
+  )
+engine=columnstore
+DEFAULT CHARACTER SET=utf8; 
